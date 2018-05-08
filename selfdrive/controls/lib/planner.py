f2a6695f7f4e70961487f9c40ec67bf9430c1a54
Joel Jacobs <joel@joeljacobs.com>
2018-05-07 18:59:34 -0600

    Tweaked accel for pedal and brake test
    Made the 5mph brake point the same as the others
    
    lower from stop accel

--- selfdrive/controls/lib/planner.py
+++ selfdrive/controls/lib/planner.py
@@ -28,13 +28,13 @@ GPS_PLANNER_ADDR = "192.168.5.1"
 
 # lookup tables VS speed to determine min and max accels in cruise
 # make sure these accelerations are smaller than mpc limits
-_A_CRUISE_MIN_V  = [-1.0, -.8, -.67, -.5, -.30]
+_A_CRUISE_MIN_V  = [-2.0, -1.4, -.67, -.5, -.30]
 _A_CRUISE_MIN_BP = [   0., 5.,  10., 20.,  40.]
 
 # need fast accel at very low speed for stop and go
 # make sure these accelerations are smaller than mpc limits
-_A_CRUISE_MAX_V = [1.1, 1.1, .8, .5, .3]
-_A_CRUISE_MAX_V_FOLLOWING = [1.6, 1.6, 1.2, .7, .3]
+_A_CRUISE_MAX_V = [1.6, 2.5, 2.5, 2.5, 2.5]
+_A_CRUISE_MAX_V_FOLLOWING = [1.0, 2.5, 2.5, 2.5, 2.5]
 _A_CRUISE_MAX_BP = [0.,  5., 10., 20., 40.]
 
 # Lookup table for turns
