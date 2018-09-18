4080161271a2df5d40b318f268b617720578040e
Joel Jacobs <joel@joeljacobs.com>
2018-09-18 11:56:17 -0600

    Experimenting with acceleration from stop

--- selfdrive/controls/lib/planner.py
+++ selfdrive/controls/lib/planner.py
@@ -30,13 +30,13 @@ GPS_PLANNER_ADDR = "192.168.5.1"
 
 # lookup tables VS speed to determine min and max accels in cruise
 # make sure these accelerations are smaller than mpc limits
-_A_CRUISE_MIN_V  = [-3.0, -2.0, -1.34, -1.0, -.50]
+_A_CRUISE_MIN_V  = [-3.0, -3.0, -2.0, -2.0, -1.0]
 _A_CRUISE_MIN_BP = [   0., 5.,  10., 20.,  40.]
 
 # need fast accel at very low speed for stop and go
 # make sure these accelerations are smaller than mpc limits
-_A_CRUISE_MAX_V = [1.6, 2.5, 2.5, 2.5, 2.5]
-_A_CRUISE_MAX_V_FOLLOWING = [1.0, 2.5, 2.5, 2.5, 2.5]
+_A_CRUISE_MAX_V = [1.6, 3.5, 3.5, 2.5, 2.5]
+_A_CRUISE_MAX_V_FOLLOWING = [1.0, 3.5, 3.5, 2.5, 2.5]
 _A_CRUISE_MAX_BP = [0.,  5., 10., 20., 40.]
 
 # Lookup table for turns
